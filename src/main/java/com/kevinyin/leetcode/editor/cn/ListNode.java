913695f6ebab92a146e097253ede3a436b61d59a
yinhao <yinhao@tuhu.cn>
2022-02-14 22:00:29 +0800

    leetcode 刷题

--- src/main/java/com/kevinyin/leetcode/editor/cn/ListNode.java
+++ src/main/java/com/kevinyin/leetcode/editor/cn/ListNode.java
@@ -1,5 +1,8 @@
 package com.kevinyin.leetcode.editor.cn;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 public class ListNode {
     public int val;
     public ListNode next;
@@ -15,4 +18,39 @@ public class ListNode {
         this.val = val;
         this.next = next;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ListNode node = (ListNode) o;
+
+        return new EqualsBuilder().append(val, node.val).append(next, node.next).isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        int nextValue = next != null ? next.val : 0;
+        return new HashCodeBuilder(17, 37).append(val).append(nextValue).toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                '}';
+    }
+
+    public static ListNode buildByArray(int[] array) {
+        ListNode node = new ListNode(array[0]);
+        ListNode pre = node;
+        for (int i = 1; i < array.length; i++) {
+            pre.next = new ListNode(array[i]);
+            pre = pre.next;
+        }
+        return node;
+    }
+
 }
