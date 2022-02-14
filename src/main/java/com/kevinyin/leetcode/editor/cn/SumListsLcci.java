bccf22c718d76cc82352f69c00b749482031d5a7
yinhao <yinhao@tuhu.cn>
2022-02-14 18:34:15 +0800

    刷

---
+++ src/main/java/com/kevinyin/leetcode/editor/cn/SumListsLcci.java
@@ -0,0 +1,79 @@
+//给定两个用链表表示的整数，每个节点包含一个数位。
+//
+// 这些数位是反向存放的，也就是个位排在链表首部。 
+//
+// 编写函数对这两个整数求和，并用链表形式返回结果。 
+//
+// 
+//
+// 示例： 
+//
+// 输入：(7 -> 1 -> 6) + (5 -> 9 -> 2)，即617 + 295
+//输出：2 -> 1 -> 9，即912
+// 
+//
+// 进阶：思考一下，假设这些数位是正向存放的，又该如何解决呢? 
+//
+// 示例： 
+//
+// 输入：(6 -> 1 -> 7) + (2 -> 9 -> 5)，即617 + 295
+//输出：9 -> 1 -> 2，即912
+// 
+// Related Topics 递归 链表 数学 👍 109 👎 0
+
+
+package com.kevinyin.leetcode.editor.cn;
+
+public class SumListsLcci {
+    public static void main(String[] args) {
+        Solution solution = new SumListsLcci().new Solution();
+        ListNode n1 = new ListNode(2);
+        ListNode n2 = new ListNode(4);
+        ListNode n3 = new ListNode(3);
+        n1.next = n2;
+        n2.next = n3;
+
+        ListNode m1 = new ListNode(5);
+        ListNode m2 = new ListNode(6);
+        ListNode m3 = new ListNode(4);
+        m1.next = m2;
+        m2.next = m3;
+
+        solution.addTwoNumbers(n1, m1);
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            ListNode node = new ListNode(0);
+            ListNode current = node;
+            int k = 0;
+            while (l1 != null || l2 != null) {
+                int sum = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + k;
+                k = sum / 10;
+                current.next = new ListNode(sum % 10);
+                current = current.next;
+                if (l1 != null) {
+                    l1 = l1.next;
+                }
+                if (l2 != null) {
+                    l2 = l2.next;
+                }
+            }
+            if (k == 1) {
+                current.next = new ListNode(k);
+            }
+            return node.next;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
